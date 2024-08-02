87e856b97c4da7fb5969435084685cde53cab29e
Henry Agustin <henryagustin297@gmail.com>
2024-08-01 21:39:30 -0600

    agregando plantilla de correo html para enviar respuesta al llenar formularios

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { Resend } from "resend";
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import {fileURLToPath} from 'url';
 const app = express()
 const port = 3000
 const api_key = "re_JyKsFqsh_Ah84MQx8NfwyJyHEj9dQ8XPR"
@@ -20,20 +23,24 @@ app.get("/api/welcomeEmail", async (req, res) => {
 
   res.status(200).json({ data });
 });
+
 app.get("/api/newClientForm", async (req, res) => {
-    
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, 'templates', `new-prospect.html`);
+  const htmlContent = readFileSync(filePath, 'utf8');
   const { data, error } = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
     to: ["henryagustin297@gmail.com"],
     subject: "Welcome, we're happy you're here.",
-    html: "<strong>Funciona!</strong>",
+    html: htmlContent,
   });
 
   if (error) {
-    return res.status(400).json({ error });
+    return res.status(400).json({ errorMessage: 'Failed to send email',error });
   }
 
-  res.status(200).json({ data });
+  res.status(200).json({ message: 'Email sent successfully',data });
 });
 
 
